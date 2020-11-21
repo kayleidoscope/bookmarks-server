e3e30f3ff7728109fd02d7a6ea801e76446358d1
Kay Holten <holtenkay@gmail.com>
2020-11-20 17:51:40 -0800

    get, post, delete work

---
+++ src/store.js
@@ -0,0 +1,9 @@
+const bookmarks = [{
+    description: "A big timesuck, but also a great invention.",
+    id: "b9fc5040-7aeb-4502-aeb5-3d569b748279",
+    rating: 5,
+    title: "Netflix",
+    url: "https://www.netflix.com/browse"
+}]
+
+module.exports = {bookmarks}
